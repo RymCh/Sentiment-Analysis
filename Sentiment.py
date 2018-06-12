9ce915c395be11ba99da27b09e5bef348695feee
Rim <chattiriim@gmail.com>
2018-06-12 16:31:47 +0100

    myfirstcommit

---
+++ Sentiment.py
@@ -0,0 +1,103 @@
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+
+
+class TwitterClient(object):
+
+	def __init__(self):
+
+		consumer_key = '62mwhUha01U6BqQJMZpgDGCJi'
+		consumer_secret = 'mBzXddkTT5BBH0EhOrhVlIKO2ku0PC0ZikY16VasSeM3vRikkn'
+		access_token = '881472440756563968-MrnraCFiUh3jJ6y2biMAOxTobHZy0rt'
+		access_token_secret = '4cHjegoO7SCjj1PyUcZQyoGNUsKPptt1ywnPDi6QVIwsw'
+
+		try:
+			self.auth = OAuthHandler(consumer_key, consumer_secret)
+			self.auth.set_access_token(access_token, access_token_secret)
+			self.api = tweepy.API(self.auth)
+		except:
+			print("Error: Authentication Failed")
+
+	def clean_tweet(self, tweet):
+		'''
+		clean tweet text by removing links, special characters
+		'''
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+	def get_tweet_sentiment(self, tweet):
+		'''
+		Utility function to classify sentiment of passed tweet
+		'''
+		# create TextBlob object of passed tweet text
+		analysis = TextBlob(self.clean_tweet(tweet))
+		# set sentiment
+		if analysis.sentiment.polarity > 0:
+			return 'positive'
+		elif analysis.sentiment.polarity == 0:
+			return 'neutral'
+		else:
+			return 'negative'
+
+	def get_tweets(self, query, count = 10):
+		'''
+		Main function to fetch tweets and parse them.
+		'''
+		# empty list to store parsed tweets
+		tweets = []
+
+		try:
+			# call twitter api to fetch tweets
+			fetched_tweets = self.api.search(q = query, count = count)
+
+			# parsing tweets one by one
+			for tweet in fetched_tweets:
+				# empty dictionary to store required params of a tweet
+				parsed_tweet = {}
+
+				# saving text of tweet
+				parsed_tweet['text'] = tweet.text
+				# saving sentiment of tweet
+				parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+
+				# appending parsed tweet to tweets list
+				if tweet.retweet_count > 0:
+					# if tweet has retweets, ensure that it is appended only once
+					if parsed_tweet not in tweets:
+						tweets.append(parsed_tweet)
+				else:
+					tweets.append(parsed_tweet)
+
+			# return parsed tweets
+			return tweets
+
+		except tweepy.TweepError as e:
+			# print error (if any)
+			print("Error : " + str(e))
+
+def main():
+	api = TwitterClient()
+	# calling function to get tweets
+	tweets = api.get_tweets(query = 'Mo Salah', count = 200)
+
+	# picking positive tweets from tweets
+	ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+	print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+	ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+	print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+	print("Neutral tweets percentage: {} % \
+		".format(100*(len(tweets) - len(ntweets) - len(ptweets))/len(tweets)))
+
+	# printing first 5 positive tweets
+	print("\n\nPositive tweets:")
+	for tweet in ptweets[:10]:
+		print(tweet['text'])
+
+	print("\n\nNegative tweets:")
+	for tweet in ntweets[:10]:
+		print(tweet['text'])
+
+if __name__ == "__main__":
+	# calling main function
+	main()
